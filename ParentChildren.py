2415ba9d8b254416ea935f332e2e71e002675c76
Micah Kemp <github@micah.ws>
2017-12-06 20:29:28 +0000

    Refactored, added tests.

--- ParentChildren.py
+++ ParentChildren.py
@@ -4,16 +4,20 @@ class Individual:
 
     def has_characteristics(self, characteristics):
         for key in characteristics:
+            #print "found %s characteristic %s" % (key, characteristics[key])
             self.characteristics.setdefault(key, set())
             if isinstance(characteristics[key], list) or isinstance(characteristics[key], set):
                 for value in characteristics[key]:
                     self.characteristics[key].add(value)
             else:
                 self.characteristics[key].add(characteristics[key])
+
+        #print "after has_characteristics %s" % self.characteristics
         
-class Parent(Individual):
+#class Parent(Individual):
+class Parent:
     def __init__(self):
-        Individual.__init__(self)
+#        Individual.__init__(self)
         self.children = set()
 
     def has_children(self, children):
@@ -21,35 +25,60 @@ class Parent(Individual):
             self.children.add(child)
 
 class Families:
-    def __init__(self):
-        self._parents = {}
-        self.individuals = {}
+    def __init__(self, parentfield='parent', childfield='child'):
+        self._parents     = {}
+        self._children    = set()
+        self._individuals = {}
+        self._parentfield = parentfield
+        self._childfield  = childfield
 
-    def individual(self, individual_name):
-        if not individual_name in self.individuals:
-            self.individuals[individual_name] = Individual()
-        return self.individuals[individual_name]
+    def process(self, event):
+        individual_name = None
 
-    def parent(self, parent_name):
+        if self._parentfield in event:
+            individual_name = event[self._parentfield]
+            parent = self.parent_with_name(individual_name)
+            if self._childfield in event:
+                children = event[self._childfield]
+                if not isinstance(children, list) or isinstance(children, set):
+                    children = [children]
+                for child in children:
+                    self._children.add(child)
+                    parent.has_children([child])
+        elif self._childfield in event:
+            individual_name = event[self._childfield]
+
+        if individual_name:
+            self.individual_with_name(individual_name).has_characteristics(event)
+
+    def individual_with_name(self, individual_name):
+        if not individual_name in self._individuals:
+            self._individuals[individual_name] = Individual()
+        return self._individuals[individual_name]
+
+    def parent_with_name(self, parent_name):
         if not parent_name in self._parents:
             self._parents[parent_name] = Parent()
         return self._parents[parent_name]
 
-    def parent_children(self, parent_name):
-        for child in self._parents[parent_name].children:
-            if child in self.individuals:
-                yield self.individuals[child]
-
-    def parents(self):
-        for parent in self._parents:
-            yield self._parents[parent]
-
-    def families(self):
+    def parent_names(self):
         for parent_name in self._parents:
+            yield parent_name
+
+    def family_units(self):
+        for parent_name in self.parent_names():
+            parent_individual = self.individual_with_name(parent_name)
+            family_unit = Individual()
             parent = self._parents[parent_name]
-            family = Individual()
-            family.has_characteristics(parent.characteristics)
+            family_unit.has_characteristics(parent_individual.characteristics)
             for child_name in parent.children:
-                child = self.individual(child_name)
-                family.has_characteristics(child.characteristics)
-            yield family
+                child = self.individual_with_name(child_name)
+                family_unit.has_characteristics(child.characteristics)
+            #print "family unit characteristics %s" % family_unit.characteristics
+            yield family_unit
+
+    def orphans(self):
+        for individual_name in self._individuals:
+            if individual_name not in self._children and individual_name not in self._parents:
+                orphan = self._individuals[individual_name]
+                yield orphan
--- test.py
+++ test.py
@@ -1,27 +1,84 @@
 import ParentChildren
 
-parentfield = 'parent'
-childfield = 'child'
-
 events = [
-    { 'parent': 'parentof_A'  , 'child': 'A',   'name': 'Parent of A'   },
-    { 'parent': 'parentof_B'  , 'child': 'B',   'name': 'Parent of B'   },
-    { 'parent': 'parentof_C_D', 'child': 'C',   'name': 'Parent of C D' },
-    { 'parent': 'parentof_C_D', 'child': 'D',   'name': 'Parent of C D' },
+    # parent has one child, but all characteristics are owned by parent
+    { 'parent': 'parentof_A'  , 'child': 'A'       , 'name': 'Parent of A'         },
+
+    # parent has one child, child's name on separate event
+    { 'parent': 'parentof_B'  , 'child': 'B'       , 'name': 'Parent of B'         },
+    {                           'child': 'B'       , 'name': 'Child of parentof_B' },
+
+    # parent has two children, each via separate event
+    { 'parent': 'parentof_C_D', 'child': 'C'       , 'name': 'Parent of C D'       },
+    { 'parent': 'parentof_C_D', 'child': 'D'       , 'name': 'Parent of C D'       },
+
+    # orphan
+    {                           'child': 'E'       , 'name': 'Child of NUL'        },
+
+    # parent has two children defined on same event
+    { 'parent': 'parentof_F_G', 'child': ['F', 'G'], 'name': 'Parent of F G'       },
+
+    # parent has one child defined later.  relationship and characteristics in separate events
+    { 'parent': 'parentof_H'  ,                      'name': 'Parent of H'         },
+    {                           'child': 'H'       , 'name': 'Child of H'          },
+    { 'parent': 'parentof_H'  , 'child': 'H'       ,                               },
+
+    # no parent or child
+    {                                                'name': 'Unrelated'           },
 ]
+
+expected_parents = {
+    'parentof_A': {
+        'parent': set(['parentof_A']),
+        'child' : set(['A']),
+        'name'  : set(['Parent of A']),
+    },
+    'parentof_B': {
+        'parent': set(['parentof_B']),
+        'child' : set(['B']),
+        'name'  : set(['Parent of B', 'Child of parentof_B']),
+    },
+    'parentof_C_D': {
+        'parent': set(['parentof_C_D']),
+        'child' : set(['C', 'D']),
+        'name'  : set(['Parent of C D']),
+    },
+    'parentof_F_G': {
+        'parent': set(['parentof_F_G']),
+        'child' : set(['F', 'G']),
+        'name'  : set(['Parent of F G']),
+    },
+    'parentof_H': {
+        'parent': set(['parentof_H']),
+        'child' : set(['H']),
+        'name'  : set(['Parent of H', 'Child of H']),
+    },
+}
+
+expected_orphans = {
+    'E': {
+        'child': set(['E']),
+        'name':  set(['Child of NUL']),
+    },
+}
 
 families = ParentChildren.Families()
 
 for event in events:
-    if parentfield in event:
-        parent = families.parent(event[parentfield])
-        parent.has_characteristics(event)
-        if childfield in event:
-            parent.has_children([event[childfield]])
-    else:
-        if childfield in event:
-            child = families.individual(event[childfield])
-            child.has_characteristics(event)
+    families.process(event)
 
-for parent in families.families():
-    print parent.characteristics
+assert( len(list(families.family_units())) == len(expected_parents) )
+for family_unit in families.family_units():
+    assert( len(family_unit.characteristics['parent']) == 1 )
+    parent_name = family_unit.characteristics['parent'].copy().pop()
+    assert( parent_name in expected_parents )
+    expected_parent = expected_parents[parent_name]
+    assert( expected_parent == family_unit.characteristics )
+
+assert( len(list(families.orphans())) == len(expected_orphans) )
+for orphan in families.orphans():
+    assert( len(orphan.characteristics['child']) == 1 )
+    orphan_name = orphan.characteristics['child'].copy().pop()
+    assert( orphan_name in expected_orphans )
+    expected_orphan = expected_orphans[orphan_name]
+    assert( expected_orphan == orphan.characteristics )
